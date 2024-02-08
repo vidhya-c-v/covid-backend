0d71eac7fff3441fd9485647e6d68044b5cdc8eb
vidhya-c-v <vidhyacv14@gmail.com>
2024-02-08 15:39:30 +0530

    view done

--- controllers/covidRouter.js
+++ controllers/covidRouter.js
@@ -1,17 +1,17 @@
-const express=require("express")
-const covidModel=require("../models/covidModel")
-const router=express.Router()
-router.post("/add",async(req,res)=>{
-    let data=req.body
-    let covid=new covidModel(data)
-    let result=await covid.save()
+const express = require("express")
+const covidModel = require("../models/covidModel")
+const router = express.Router()
+router.post("/add", async (req, res) => {
+    let data = req.body
+    let covid = new covidModel(data)
+    let result = await covid.save()
     res.json({
-        status:"success"
+        status: "success"
     })
+})
 router.get("/view",async(req,res)=>{
     let data=await covidModel.find()
     res.json(data)
 })
 
-})
-module.exports=router
+module.exports = router
