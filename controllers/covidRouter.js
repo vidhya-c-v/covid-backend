d22f9e3c801b2c9fe44a7da525eca18bd62922ab
vidhya-c-v <vidhyacv14@gmail.com>
2024-02-08 15:20:26 +0530

    entered patient details

---
+++ controllers/covidRouter.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const covidModel=require("../models/covidModel")
+const router=express.Router()
+router.post("/add",async(req,res)=>{
+    let data=req.body
+    let covid=new covidModel(data)
+    let result=await covid.save()
+    res.json({
+        status:"success"
+    })
+router.get("/view",async(req,res)=>{
+    let data=await covidModel.find()
+    res.json(data)
+})
+
+})
+module.exports=router
